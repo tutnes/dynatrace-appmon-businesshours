05d1edd695b76ce60a1ce807d93f309fa5a4e092
Tarjei Utnes <tutnes@gmail.com>
2018-01-04 15:20:38 +0100

    Initial commit

---
+++ createforall.js
@@ -0,0 +1,104 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; //Needed because of wrong Cipher 
+var http = require("https");
+
+var username = 'admin';
+var password = 'admin';
+var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+
+
+var options = {
+  "method": "GET",
+  "hostname": "localhost",
+  "port": "8021",
+  "path": "/api/v2/profiles",
+  "headers": {
+    "content-type": "application/json",
+    "authorization": auth
+  }
+
+};
+
+var exampleHour2 = {
+  "businesshoursName": "ExcludeDaily 06:50-07:10",
+  "body": {
+    "exclusions": [{
+        "timespan": {
+        "start": "06:50",
+        "end": "07:10"
+      }
+    }]
+  }
+};
+
+var exampleHour = {
+  "businesshoursName": "ExcludeDaily 06:50-07:10",
+  "body": {
+    "inclusions": [{
+      "timespan": {
+        "start": "07:40",
+        "end": "19:50"
+      }
+    }]
+  }
+};
+
+
+function getBusinessHours(profile) {
+  let path = encodeURI("/api/v2/profiles/" + d + "/businesshours");
+  options.path = path;
+  invokeRest(options, logOutput);
+}
+
+function getBusinessHour(profile, name) {
+  let path = encodeURI("/api/v2/profiles/" + profile + "/businesshours/" + name);
+  options.path = path;
+  invokeRest(options, logOutput);
+}
+
+function putBusinessHour(profile, name, body) {
+  let path = encodeURI("/api/v2/profiles/" + profile + "/businesshours/" + name);
+  options.method = "PUT";
+  options.path = path;
+  options.body = body;
+  invokeRest(options, logOutput);
+}
+
+function deleteBusinessHour(profile, name) {
+  let path = encodeURI("/api/v2/profiles/" + profile + "/businesshours/" + name);
+  options.method = "DELETE";
+  options.path = path;
+  invokeRest(options, logOutput);
+}
+
+
+function logOutput(d) {
+  console.log(d);
+}
+
+function getProfiles(d) {
+  for (let i = 0; i < d.systemprofiles.length; i++) {
+    deleteBusinessHour(d.systemprofiles[i].id,exampleHour2.businesshoursName);
+    //putBusinessHour(d.systemprofiles[i].id,exampleHour2.businesshoursName,exampleHour2.body);
+    //getBusinessHours(d.systemprofiles[i].id);
+  }
+}
+
+function invokeRest(options, cb) {
+  var req = http.request(options, function(res) {
+    var chunks = [];
+    res.on("data", function(chunk) {
+      chunks.push(chunk);
+    });
+    res.on("end", function() {
+      var body = Buffer.concat(chunks);
+      var d = JSON.parse(body.toString());
+      cb(d);
+    });
+  });
+  if (options.body) {
+    req.write(JSON.stringify(options.body));
+  }
+  req.end();
+}
+
+invokeRest(options, getProfiles);
---
+++ createforall.ps1
@@ -0,0 +1,47 @@
+﻿$netAssembly = [Reflection.Assembly]::GetAssembly([System.Net.Configuration.SettingsSection])
+
+if($netAssembly)
+{
+    $bindingFlags = [Reflection.BindingFlags] "Static,GetProperty,NonPublic"
+    $settingsType = $netAssembly.GetType("System.Net.Configuration.SettingsSectionInternal")
+
+    $instance = $settingsType.InvokeMember("Section", $bindingFlags, $null, $null, @())
+
+    if($instance)
+    {
+        $bindingFlags = "NonPublic","Instance"
+        $useUnsafeHeaderParsingField = $settingsType.GetField("useUnsafeHeaderParsing", $bindingFlags)
+
+        if($useUnsafeHeaderParsingField)
+        {
+          $useUnsafeHeaderParsingField.SetValue($instance, $true)
+        }
+    }
+}
+
+
+#[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true} ;
+$Cred = Get-Credential
+
+# Next, allow the use of self-signed SSL certificates.
+
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $True }
+
+# Create variables to store the values consumed by the Invoke-RestMethod command.
+# The search variable contents are later embedded in the body variable.
+
+$Server = '127.0.0.1'
+$Url = "https://${server}:8021/api/v2/profiles"
+#$Search = "search index=_internal | reverse | table index,host,source,sourcetype,_raw"
+
+# The cmdlet handles URL encoding. The body variable describes the search criteria, specifies CSV as the output mode, and specifies a time period for returned data that starts two days ago and ends one day ago. The body variable specifies values for parameters that apply to the particular REST API with which Invoke-RestMethod is communicating.
+
+
+$wc = New-Object System.Net.WebClient
+
+$wc.DownloadString("https://localhost:8021/api/v2/profiles")
+
+# Now, run the Invoke-RestMethod command with all variables in place, specifying a path and file name for the resulting CSV output file.
+
+#Invoke-WebRequest -Uri $url
+Invoke-RestMethod -Method GET -Uri $url -Credential $Cred #-Body $body #-OutFile output.csv
